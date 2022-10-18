7c8c22f53edcf3e45cebe21b5ab2b0d1506641d3
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-18 17:31:18 +0530

    added UC-1 option to switch case in main method

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LineComparisonNonStatic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Use Case #1: Model a Line and Calculate Length");
+
+            Console.Write("Choose a use case number to run the program: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    ModelALine line = new ModelALine();
+                    double lineLength = line.LineLenght();
+                    Console.WriteLine("Length of line: " + lineLength);
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid use case number!");
+                    break;
+            }
+        }
+    }
+}
