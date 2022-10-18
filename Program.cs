d196bdb9f305740f785770ddf21dcb979c7bf81e
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-18 17:53:54 +0530

    added UC-3 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LineComparisonNonStatic
+﻿using System;
+
+namespace LineComparisonNonStatic
 {
     class Program
     {
@@ -7,6 +9,7 @@
             Console.WriteLine("Welcome to Line Comparison Computation Program");
             Console.WriteLine("Use Case #1: Model a Line and Calculate Length");
             Console.WriteLine("Use Case #2: Check Equality of Two Lines");
+            Console.WriteLine("Use Case #3: Compare Two Lines");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -18,9 +21,13 @@
                     double lineLength = line.LineLenght();
                     Console.WriteLine("Length of line: " + lineLength);
                     break;
-                    case 2:
+                case 2:
                     CheckEquality checkEquality = new CheckEquality();
                     checkEquality.CompareTwoLines();
+                    break;
+                case 3:
+                    CompareTwoLines compareTwoLines = new CompareTwoLines();
+                    compareTwoLines.CompareLines();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
